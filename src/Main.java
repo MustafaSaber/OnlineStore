f44c5220a510cf3c3375ce7e9cd8701ae18031e4
Mostafa Saber <mostafa.saber3789@gmail.com>
2017-12-08 13:45:55 +0200

    try branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int x = 4;
+        System.out.println("Hello World! " +  x);
     }
 }
