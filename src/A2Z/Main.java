896804e2fbfde07fd878e7df5c45d4c9dfc1e2a2
NardeenMokhless <nardeenmokhless11@gmail.com>
2017-12-08 21:42:30 +0200

    nardeen

--- src/A2Z/Main.java
+++ src/A2Z/Main.java
@@ -6,6 +6,7 @@ import java.util.Vector;
  * Created by Nardeen on 08-Dec-17.
  */
 public class Main {
+
     public static void main(String[] args) {
     }
 }
--- src/A2Z/StoreOwner.java
+++ src/A2Z/StoreOwner.java
@@ -5,5 +5,45 @@ import java.util.Vector;
 public class StoreOwner {
     private Vector<Store> stores;
 
+    public StoreOwner() {
+    }
 
+    public StoreOwner(Vector<Store> stores) {
+        this.stores = stores;
+    }
+
+    public Vector<Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(Vector<Store> stores) {
+        this.stores = stores;
+    }
+
+    public boolean AddStore(String name,String StoreId)
+    {
+        Store s = new Store(name,StoreId);
+        stores.add(s);
+        return true;
+    }
+    public boolean RemoveStore(String name,String StoreId)
+    {
+        for (Store store : stores)
+        {
+            if (store.getName() == name && store.getStoreID() == StoreId)
+            {
+                stores.remove(store);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void SuggestProduct()
+    {
+
+    }
+    public void SuggestBrand()
+    {
+
+    }
 }
