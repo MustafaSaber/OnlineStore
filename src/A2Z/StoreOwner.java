0ed26be00d51a1d268f3a6779d5120a5ba229393
NardeenMokhless <nardeenmokhless11@gmail.com>
2017-12-08 22:28:03 +0200

    nardeen

--- src/A2Z/StoreOwner.java
+++ src/A2Z/StoreOwner.java
@@ -1,15 +1,20 @@
 package A2Z;
 
+import java.util.Scanner;
 import java.util.Vector;
 
-public class StoreOwner {
+public class StoreOwner extends User {
     private Vector<Store> stores;
 
-    public StoreOwner() {
-    }
 
     public StoreOwner(Vector<Store> stores) {
         this.stores = stores;
+    }
+    public StoreOwner(String nm, String email, String usernm, String pass) {
+        this.setName(nm);
+        this.setEmail(email);
+        this.setUsername(usernm);
+        this.setPassword(pass);
     }
 
     public Vector<Store> getStores() {
@@ -24,6 +29,7 @@ public class StoreOwner {
     {
         Store s = new Store(name,StoreId);
         stores.add(s);
+
         return true;
     }
     public boolean RemoveStore(String name,String StoreId)
@@ -40,10 +46,67 @@ public class StoreOwner {
     }
     public void SuggestProduct()
     {
-
+        //fadi
     }
-    public void SuggestBrand()
+    public void SuggestBrand(String name,String id)
     {
-
+        Brand b = new Brand(name,id);
+        System.suggestBrands.add(b);
+    }
+    public String ViewStat()
+    {
+        String stat = "";
+        return stat;
+    }
+    public void AddProductToStore(Store s , Product p)
+    {
+        s.AddProduct(p);
+    }
+    public boolean AddStoreOwnerToDB(String name ,String email,String usernm ,String pass)
+    {
+        StoreOwner so = new StoreOwner(name,email,usernm,pass);
+        System.storeOwners.add(so);
+        return true;
+    }
+    public boolean RemoveStoreOwnerToDB(String Uname)
+    {
+        //search for storeowner
+        for(StoreOwner so :System.storeOwners)
+        {
+            if(so.getUsername() == Uname)
+            {
+                System.storeOwners.remove(so);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean UpdateStoreOwnerToDB(String Uname)
+    {
+        for(StoreOwner so :System.storeOwners)
+        {
+            if(so.getUsername() == Uname)
+            {
+                Scanner sc = new Scanner(java.lang.System.in);
+                java.lang.System.out.print("Enter new name:");
+                String n = sc.nextLine();
+                so.setName(n);
+                java.lang.System.out.print("Enter new email:");
+                n = sc.nextLine();
+                so.setEmail(n);
+                java.lang.System.out.print("Enter new username:");
+                n = sc.nextLine();
+                so.setUsername(n);
+                java.lang.System.out.print("Enter new password:");
+                n = sc.nextLine();
+                so.setPassword(n);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean Verify(String address)
+    {
+        return true;
     }
 }
