b060c420379760a860cd493df35deb907062e1d0
Mina Yousry <menayousry60@hotmail.com>
2017-12-08 17:20:54 +0200

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int x = 4;
-        int f = 5;
+        int f = 4;
         System.out.println("Hello World! " +  x + " " + f);
         System.out.println("Hello World! test to make commit");
 
