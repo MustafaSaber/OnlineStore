a5319466e78ee5dc58a61895df577637ef477b0c
Mina Yousry <menayousry60@hotmail.com>
2017-12-08 20:35:24 +0200

    Mina Normal

--- src/A2Z/NormalCustomer.java
+++ src/A2Z/NormalCustomer.java
@@ -3,6 +3,32 @@ package A2Z;
 /**
  * Created by Mina_Yousry on 08/12/2017.
  */
-public class NormalCustomer {
+public class NormalCustomer  extends User {
+
+    public NormalCustomer() {
+
+    }
+
+    public NormalCustomer(String name, String email,
+                          String username, String password) {
+        super(name, email, username, password);
+    }
+
+    boolean AddUserToDb()
+    {
+        return false;
+    }
+
+    boolean UpdateUserInDb()
+    {
+        return false;
+    }
+
+
+    boolean RemoveUserFromDB()
+    {
+        return  false;
+
+    }
 
 }
