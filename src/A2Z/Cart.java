7406d0a428b2d6cffc70c978a94564c91f3a3c22
Mostafa Saber <mostafa.saber3789@gmail.com>
2017-12-11 12:35:12 +0200

    implement some cart functions.

--- src/A2Z/Cart.java
+++ src/A2Z/Cart.java
@@ -26,6 +26,7 @@ public class Cart {
     /**
      * 
      */
+
     private String address;
 
     /**
@@ -38,7 +39,51 @@ public class Cart {
      */
     private double TotalPrice;
 
+    public Cart(String address, double totalPrice) {
+        this.products = new Vector<>();
+        this.creditcard = new CreditCard();
+        this.address = address;
+        this.Vouchercard = new VoucherCard();
+        TotalPrice = totalPrice;
+    }
 
+    public Vector<Product> getProducts() {
+        return products;
+    }
 
+    public void setProducts(Vector<Product> products) {
+        this.products = products;
+    }
 
+    public CreditCard getCreditcard() {
+        return creditcard;
+    }
+
+    public void setCreditcard(CreditCard creditcard) {
+        this.creditcard = creditcard;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public VoucherCard getVouchercard() {
+        return Vouchercard;
+    }
+
+    public void setVouchercard(VoucherCard vouchercard) {
+        Vouchercard = vouchercard;
+    }
+
+    public double getTotalPrice() {
+        return TotalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        TotalPrice = totalPrice;
+    }
 }
