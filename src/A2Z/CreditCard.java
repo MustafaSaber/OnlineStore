134667a040798e029feb827f2de20d216692cfe6
Mostafa Saber <mostafa.saber3789@gmail.com>
2017-12-11 22:34:56 +0200

    add attribute

--- src/A2Z/CreditCard.java
+++ src/A2Z/CreditCard.java
@@ -2,13 +2,23 @@ package A2Z;
 
 public class CreditCard {
     private String cardID;
+    private double myMoney;
+
+    public double getMyMoney() {
+        return myMoney;
+    }
+
+    public void setMyMoney(double myMoney) {
+        this.myMoney = myMoney;
+    }
 
     public CreditCard(){
 
     }
 
-    public CreditCard(String cardID) {
+    public CreditCard(String cardID , double mo) {
         this.cardID = cardID;
+        myMoney = mo;
     }
 
     public String getCardID() {
